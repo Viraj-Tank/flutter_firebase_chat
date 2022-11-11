40f0c1d457def24942dc2252b976cdfe1876185f
Viraj Tank <virajtank98@gmail.com>
2022-11-11 11:54:54 +0530

    - setup firebase

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -14,6 +15,17 @@ class ChatScreen extends StatelessWidget {
           return Text('Tile Number ${index + 1}');
         },
         itemCount: 10,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          FirebaseFirestore.instance.collection('').snapshots()
+              .listen((data) {
+            for (var element in data.docs) {
+              print('printlog ${element['text']}');
+            }
+          });
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
